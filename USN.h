ae2c1a087704cf121b92772831efb83dfe35f9ef
FangWHao <39217913+FangWHao@users.noreply.github.com>
2020-12-01 17:25:56 +0800

    Add files via upload

---
+++ USN.h
@@ -0,0 +1,192 @@
+#define UNICODE
+#define _UNICODE
+#define WIN32_LEAN_AND_MEAN
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <cstdio>
+#include <windef.h>
+#include <winioctl.h>
+#include <windows.h>
+#include <vector>
+#include <deque>
+#include <fstream>
+using namespace std;
+#define BUF_LEN 4096
+#define ull unsigned long long
+struct nod{
+	char* filename;
+	int len;
+	int rnum, prnum,ppos;
+	nod(){}
+	nod(char* s,int r,int pr=0,int l=0,int pp=0):rnum(r),prnum(pr),len(l),ppos(pp){
+		filename=new char[l];
+		strcpy(filename,s);
+	}
+};
+struct MY_USN_RECORD
+{
+	DWORDLONG FileReferenceNumber;
+	DWORDLONG ParentFileReferenceNumber;
+	LARGE_INTEGER TimeStamp;
+	DWORD Reason;
+	WCHAR FileName[MAX_PATH];
+	USN usn;
+};
+USN GET_MFT(string data_dst,string s,bool flag,vector<nod>&v)
+{
+	/*获取驱动句柄*/
+	s="\\\\.\\"+s+":";
+	const char* dv=s.c_str();
+	HANDLE hcj = CreateFile(dv, GENERIC_READ | GENERIC_WRITE,
+							FILE_SHARE_READ | FILE_SHARE_WRITE,
+							NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
+	if (INVALID_HANDLE_VALUE == hcj)
+		return -1;
+	/*初始化USN日志文件*/
+	CREATE_USN_JOURNAL_DATA cujd;
+	cujd.MaximumSize = 0;
+	cujd.AllocationDelta = 0;
+	bool ret;
+	DWORD br;
+	ret = DeviceIoControl(hcj,
+						  FSCTL_CREATE_USN_JOURNAL,
+						  &cujd,
+						  sizeof(cujd),
+						  NULL,
+						  0,
+						  &br,
+						  NULL);
+	bool initUsnJournalSuccess;
+	if (0 == ret)
+		return -1;
+	/*获取USN文件的基本信息*/
+	bool getBasicInfoSuccess = false;
+	USN_JOURNAL_DATA UsnInfo;
+	ret = DeviceIoControl(hcj,
+						  FSCTL_QUERY_USN_JOURNAL,
+						  NULL,
+						  0,
+						  &UsnInfo,
+						  sizeof(UsnInfo),
+						  &br,
+						  NULL);
+	if (0 == ret)
+		return -1;
+	if(flag)return UsnInfo.UsnJournalID;
+	MFT_ENUM_DATA med;
+	med.StartFileReferenceNumber = 0;
+	med.LowUsn = 0; //UsnInfo.FirstUsn;
+	med.HighUsn = UsnInfo.NextUsn;
+	CHAR buffer[BUF_LEN]; //用于储存记录的缓冲,尽量足够地大
+	DWORD usnDataSize;
+	PUSN_RECORD UsnRecord;
+	long long cnt = 0;
+	std::ofstream fout(data_dst, std::ios::binary);
+	while (0 != DeviceIoControl(hcj,
+								FSCTL_ENUM_USN_DATA,
+								&med,
+								sizeof(med),
+								buffer,
+								BUF_LEN,
+								&usnDataSize,
+								NULL))
+	{
+		DWORD dwRetBytes = usnDataSize - sizeof(USN);
+		UsnRecord = (PUSN_RECORD)(((PCHAR)buffer) + sizeof(USN));
+		while (dwRetBytes > 0)
+		{
+			int strLen = UsnRecord->FileNameLength;
+			char fileName[MAX_PATH] = {0};
+			WideCharToMultiByte(CP_OEMCP, NULL, UsnRecord->FileName, strLen / 2, fileName, strLen, NULL, FALSE);
+			int rnum=(int)UsnRecord->FileReferenceNumber,prnum=(int)UsnRecord->ParentFileReferenceNumber;
+			int len=strlen(fileName)+1;
+			fout.write((char*)&rnum, sizeof(int));
+        	fout.write((char*)&prnum, sizeof(int));
+        	fout.write((char*)&len, sizeof(int));
+        	fout.write(fileName, sizeof(char) * len);
+			v.emplace_back(fileName,(int)UsnRecord->FileReferenceNumber,(int)UsnRecord->ParentFileReferenceNumber,len);
+			char *pathBuffer[BUF_LEN];
+			DWORD recordLen = UsnRecord->RecordLength;
+			dwRetBytes -= recordLen;
+			UsnRecord = (PUSN_RECORD)(((PCHAR)UsnRecord) + recordLen);
+		}
+		med.StartFileReferenceNumber = *(USN *)&buffer;
+	}
+	int end=-1;
+	fout.write((char*)&end,sizeof(int));
+	CloseHandle(hcj);
+	fout.close();
+	return UsnInfo.UsnJournalID;
+}
+bool is_ntfs(const char* drv){
+	bool ret=0;
+	HANDLE hVol = INVALID_HANDLE_VALUE;
+	char FileSystemName[MAX_PATH+1];
+	DWORD MaximumComponentLength;
+	if(GetVolumeInformationA((std::string(drv)+":\\").c_str(),0,0,0,&MaximumComponentLength,0,FileSystemName,MAX_PATH+1)&&0==strcmp(FileSystemName,"NTFS"))ret=1;
+	CloseHandle(hVol);
+	return ret;
+}
+USN GET_USN(const char* drvname,USN startUSN,bool flag,vector<int>&rm,vector<nod>&cr,vector<string>&new_name,int* renamed)
+{
+	int cnt=0;
+	HANDLE hVol = INVALID_HANDLE_VALUE;
+	USN ret=0;
+	char FileSystemName[MAX_PATH+1];
+	DWORD MaximumComponentLength;
+	if( GetVolumeInformationA( (std::string(drvname)+":\\").c_str(),0,0,0,&MaximumComponentLength,0,FileSystemName,MAX_PATH+1)
+		&& 0==strcmp(FileSystemName,"NTFS") ) // 判断是否为 NTFS 格式
+	{
+		hVol = CreateFileA( (std::string("\\\\.\\")+drvname+":").c_str() // 需要管理员权限，无奈
+			, GENERIC_READ|GENERIC_WRITE, FILE_SHARE_READ|FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+		if( hVol != INVALID_HANDLE_VALUE )
+		{
+			DWORD br;
+			USN_JOURNAL_DATA qujd;
+			if( DeviceIoControl( hVol, FSCTL_QUERY_USN_JOURNAL, NULL, 0, &qujd, sizeof(qujd), &br, NULL ) )
+			{
+				char buffer[0x1000];
+				DWORD BytesReturned;
+				{
+					READ_USN_JOURNAL_DATA rujd = { startUSN, -1, 0, 0, 0, qujd.UsnJournalID };
+					for( ; DeviceIoControl(hVol,FSCTL_READ_USN_JOURNAL,&rujd,sizeof(rujd),buffer,_countof(buffer),&BytesReturned,NULL); rujd.StartUsn=*(USN*)&buffer )
+					{
+						DWORD dwRetBytes = BytesReturned - sizeof(USN);
+						PUSN_RECORD UsnRecord = (PUSN_RECORD)((PCHAR)buffer+sizeof(USN));
+						if( dwRetBytes==0 )break;
+						while( dwRetBytes > 0 )
+						{
+							const int strLen = UsnRecord->FileNameLength;
+							char fileName[MAX_PATH] = {0};
+							WideCharToMultiByte(CP_OEMCP, NULL, UsnRecord->FileName, strLen / 2, fileName, strLen, NULL, FALSE);
+							ret=UsnRecord->Usn;
+							//Reason 256 表示创建 4096 表示删除
+							if(flag){
+								if(UsnRecord->Reason==256)
+									cr.emplace_back(fileName,(int)UsnRecord->FileReferenceNumber,(int)UsnRecord->ParentFileReferenceNumber,strLen+1,0);
+								else if(UsnRecord->Reason==4096)
+									rm.push_back((int)UsnRecord->FileReferenceNumber);
+								else if(UsnRecord->Reason==8192){
+									int tmp=renamed[(int)UsnRecord->FileReferenceNumber];
+									if(!tmp){
+										renamed[(int)UsnRecord->FileReferenceNumber]=++cnt;
+										tmp=cnt;
+										new_name.push_back(fileName);
+									}
+									else new_name[tmp-1]=fileName;
+								}
+							}
+							dwRetBytes -= UsnRecord->RecordLength;
+							UsnRecord = (PUSN_RECORD)( (PCHAR)UsnRecord + UsnRecord->RecordLength);
+						}
+					}
+				}
+			}
+			CloseHandle( hVol );
+		}
+	}
+	
+	return ret;
+}
---
+++ main.cpp
@@ -0,0 +1,249 @@
+#include <iostream>
+#include <cstdio>
+#include <windows.h>
+#include <cstdlib>
+#include <ShlObj.h>
+#include <fstream>
+#include <direct.h>
+#include <sstream>
+#include <streambuf>
+#include <map>
+#include <deque>
+#include <vector>
+#include <time.h>
+#include "USN.h"
+#include "reg_match.h"
+using namespace std;
+#define ull unsigned long long
+#define MAXN 10000000
+int disk_num=0;
+USN last_USN[255];//读取到的上一次运行时各个硬盘的最后一个USN编号
+USN USN_ID[255]; //每一个硬盘中的USN日志编号
+bool flag_rescanned[30];//标记该磁盘是否被重新扫描过
+vector<int>rm_pos[30];  //被删除的文件在文件中的位置
+vector<int>rm,tmp_ull;  //被删除文件的USN编号
+vector<nod>data[30],tmp_nod,crt,rnm;//保存的文件所有信息，创建的文件信息和重命名的信息
+vector<string>new_name;//保存一个被重命名文件的最终名字
+bool removed[MAXN],exist[MAXN];//标记是否被删除，是否已经存在
+int renamed[MAXN];//被重命名的文件的USN编号->new_name中的编号
+project match;
+void exit()
+{
+	printf("Press any key to exit");
+	getchar();
+	exit(0);
+}
+void admincheck()//检查是否以管理员权限运行
+{
+	bool flag = IsUserAnAdmin();
+	if (!flag)
+	{
+		printf("Fatal Error : Please run as Administrator.\n");
+		exit();
+	}
+}
+bool check_local(){
+	//检查本地文件是否存在，此处为简便只检查setting和info
+	fstream _file;
+	_file.open("database\\settings.db", ios::in);
+	if (!_file)
+		return 1;
+	_file.close();
+	_file.open("database\\info.db", ios::in);
+	if (!_file)
+		return 1;
+	_file.close();
+	return 0;
+}
+
+void read_settings(){//读取本地存储的设置文件
+
+}
+int check_disk(){//检查本地磁盘数量
+	//可能存在的潜在bug:ntfs格式磁盘不连续时会跳过末尾的磁盘
+	int tot=0;
+	for(char dst[]="C";;dst[0]++){
+		if(is_ntfs(dst))tot++;
+		else break;
+	}
+	return tot;
+}
+USN write_MFT(const char* disk_path,const char* data_dst,vector<nod>&v){
+	//输出指定磁盘的所有信息并返回该磁盘USN的ID
+	USN now_USN;
+	now_USN=GET_MFT(data_dst,disk_path,0,v);
+	return now_USN;
+}
+void read_info(){
+	freopen("database\\info.db","r",stdin);
+	cin>>disk_num;
+	for(int i=0;i<disk_num;i++)
+		cin>>USN_ID[i];
+	for(int i=0;i<disk_num;i++)
+		cin>>last_USN[i];
+	freopen("CON", "r", stdin);
+}
+bool check_info(){
+	//检查信息是否合理
+	bool flag=0;
+	char disk_path[]="C",data_dst[]="database\\c.db";
+	for(int i=0;i<disk_num;i++,disk_path[0]++,data_dst[9]++){
+		USN now_USN=GET_MFT(data_dst,disk_path,1,tmp_nod);
+		if(now_USN!=USN_ID[i]){
+			//若现有的USN的ID与上一次运行时的ID不符，说明USN日志遭到了改动
+			USN_ID[i]=now_USN;
+			flag_rescanned[i]=1;
+			printf("Fatal Error: Change in USN Journal of %s detected. Please wait the program to re-scan the disk. This may take several minutes.\n",disk_path);
+			printf("Rescanning %s: please wait.\n",disk_path);
+			write_MFT(disk_path,data_dst,data[i]);
+			printf("Successfully rescanned %s.\n",disk_path);
+		}
+	}
+}
+bool update_info(){
+	freopen("database\\info.db","w",stdout);
+	cout<<disk_num<<endl;
+	for(int i=0;i<disk_num;i++)
+		cout<<USN_ID[i]<<endl;
+	char disk_path[]="C";
+	for(int i=0;i<disk_num;i++)
+		cout<<last_USN[i]<<endl;
+	freopen("CON", "w", stdout);
+}
+void first_run(){
+	mkdir("database");//创建文件夹
+	/****************Debug************/
+	freopen("database\\settings.db","w",stdout);
+	cout<<"Test"<<endl;
+	freopen("CON","w",stdout);
+	char disk_path[] ="C",data_dst[]="database\\c.db";
+	disk_num=check_disk();
+	for(int i=0;i<disk_num;i++,disk_path[0]++,data_dst[9]++){
+		flag_rescanned[i]=1;
+		USN_ID[i]=write_MFT(disk_path,data_dst,data[i]);
+		last_USN[i]=GET_USN(disk_path,0,0,tmp_ull,tmp_nod,new_name,renamed);
+		printf("Successfully scanned %s:\n", disk_path);
+	}
+}
+void update_database(){
+	char disk_path[]="C";
+	char data_dst[] = "database\\c.db";
+	for(int x=0;x<disk_num;x++,disk_path[0]++,data_dst[9]++){
+		printf("Now updating disk %s, please wait.\n",disk_path);
+		if(flag_rescanned[x])continue;
+		crt.clear();
+		last_USN[x]=GET_USN(disk_path,last_USN[x],1,rm,crt,new_name,renamed);
+		register nod buffer;
+		ifstream fin(data_dst, std::ios::binary);
+		while(1){
+			fin.read((char*)&buffer.rnum, sizeof(int));
+			if(buffer.rnum==-1)break;
+			fin.read((char*)&buffer.prnum, sizeof(int));
+			fin.read((char*)&buffer.len, sizeof(int));
+			buffer.filename=new char[buffer.len];
+			fin.read(buffer.filename, sizeof(char) * buffer.len);
+			if(!removed[buffer.rnum]){
+				data[x].push_back(buffer);
+				exist[buffer.rnum]=1;
+			}
+		}
+		fin.close();
+		cout << "read database: " << (double)clock() /CLOCKS_PER_SEC<< "s" << endl;
+		for(int i=0;i<crt.size();i++){
+			if(!removed[crt[i].rnum]&&!exist[crt[i].rnum]){
+				data[x].push_back(crt[i]);
+				exist[crt[i].rnum]=1;
+			}
+		}
+		register int* tmp;
+		tmp=new int[MAXN];
+		memset(tmp,0,sizeof(tmp));
+		cout << "read usn : " << (double)clock() /CLOCKS_PER_SEC<< "s" << endl;
+		ofstream fout(data_dst, std::ios::binary);
+		for(register int i=0;i<data[x].size();i++){
+			if(renamed[data[x][i].rnum]){
+				register int nlen=new_name[renamed[data[x][i].rnum]-1].length();
+				delete[] data[x][i].filename;
+				data[x][i].filename=new char[nlen+1];
+				data[x][i].len=nlen+1;
+				strcpy(data[x][i].filename,new_name[renamed[data[x][i].rnum]-1].c_str());
+				*(data[x][i].filename+nlen)='\0';
+			}
+			fout.write((char*)&data[x][i].rnum, sizeof(int));
+         	fout.write((char*)&data[x][i].prnum, sizeof(int));
+         	fout.write((char*)&data[x][i].len, sizeof(int));
+         	fout.write(data[x][i].filename, sizeof(char) * data[x][i].len);
+			tmp[data[x][i].rnum]=i;
+		}
+		int end=-1;
+		fout.write((char*)&end,sizeof(int));
+		fout.close();
+		cout << "write database : " << (double)clock() /CLOCKS_PER_SEC<< "s" << endl;
+		/***建立文件索引路径***/
+		for(int i=0;i<data[x].size();i++){
+			data[x][i].ppos=tmp[data[x][i].prnum];
+		}
+		delete[] tmp;
+		/***初始化***/
+		rm.clear();
+		crt.clear();
+		new_name.clear();
+		memset(removed,0,sizeof(removed));
+		memset(renamed,0,sizeof(renamed));
+		memset(exist,0,sizeof(exist));
+		printf("Successfully updated disk %s.\n",disk_path);
+	}
+}
+string get_path(int x,int pos){
+	string path;
+	path=data[x][pos].filename;
+	register int fa=data[x][pos].ppos;
+	while(1){
+		path="\\"+path;
+		path=data[x][fa].filename+path;
+		fa=data[x][fa].ppos;
+		if(fa==0)break;
+	}
+	return path;
+}
+int main(){
+	admincheck();
+	bool is_first_run=check_local();//若本地文件不存在，则判定为首次运行
+	/*初始化信息*/
+	//is_first_run=1;
+	//cin>>is_first_run;
+	if(is_first_run){
+		printf("This is your first run, program needs some time to initialize.\n");
+		printf("Scanning, please wait several minutes.\n");
+	}
+	else {//判定是否出现不合法信息，若出现则当作第一次运行处理
+		read_settings();//读入设置信息
+		read_info();    //读入USN64位ID和上次的最后一个USN
+		if(disk_num!=check_disk()){ //磁盘数量改变属于严重错误，应重新初始化
+			cout<<"Fatal Error:Change in disk number detected.";
+			is_first_run=1;
+		}
+		else {
+			check_info(); //检查每块磁盘的USN64位ID是否改变，若改变则更新一下
+		}
+	}
+	if(is_first_run)first_run();
+	else update_database();//先分析USN记录，记录要删除的文件再在读入时标记
+	update_info();
+	cout << "Totle Time : " << (double)clock() /CLOCKS_PER_SEC<< "s" << endl;
+	puts("\n\n\n\n");
+	cout<<"Input any word to start searching\n";
+	while(1){
+		match.read_reg(500,0);
+		char disk[]="C:";
+		for(int x=0;x<disk_num;x++,disk[0]++){
+			for(int i=0;i<data[x].size();i++){
+				if(match.match(data[x][i].filename)){
+					cout<<disk<<'\\';
+					cout<<get_path(x,i)<<endl;
+					//getchar();
+				}
+			}
+		}
+	}
+}
