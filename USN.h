297b2cbe7e9fa88ee3e954a3138741ca58948b63
FangWHao <39217913+FangWHao@users.noreply.github.com>
2020-12-18 11:59:28 +0800

    Update USN.h

--- USN.h
+++ USN.h
@@ -34,50 +34,53 @@ USN GET_MFT(string data_dst, string s, bool flag, vector<dat> &v)
 	s = "\\\\.\\" + s + ":";
 	const char *dv = s.c_str();
 	HANDLE hcj = CreateFile(dv, GENERIC_READ | GENERIC_WRITE,
-							FILE_SHARE_READ | FILE_SHARE_WRITE,
-							NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
+							FILE_SHARE_READ | FILE_SHARE_WRITE,						//CreateFile只能打开已经存在的对象
+							NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL); //忽略文件安全选项
 	if (INVALID_HANDLE_VALUE == hcj)
 		return -1;
+
 	/*初始化USN日志文件*/
-	CREATE_USN_JOURNAL_DATA cujd;
+	CREATE_USN_JOURNAL_DATA cujd; //创建USN日志数据
 	cujd.MaximumSize = 0;
 	cujd.AllocationDelta = 0;
 	bool ret;
 	DWORD br;
 	ret = DeviceIoControl(hcj,
-						  FSCTL_CREATE_USN_JOURNAL,
-						  &cujd,
-						  sizeof(cujd),
+						  FSCTL_CREATE_USN_JOURNAL, //CTL_CODE 创建USN日志，若有则返回已有的
+						  &cujd,					//Input Buffer
+						  sizeof(cujd),				//Input Buffer size
 						  NULL,
 						  0,
-						  &br,
+						  &br, //实际输出数据的bytes
 						  NULL);
-	bool initUsnJournalSuccess;
 	if (0 == ret)
 		return -1;
+
 	/*获取USN文件的基本信息*/
-	bool getBasicInfoSuccess = false;
-	USN_JOURNAL_DATA UsnInfo;
+	USN_JOURNAL_DATA UsnInfo; //USN日志的基础信息，包括64位ID，最小最大USN等信息（边界），在winioctl.h中可见定义
 	ret = DeviceIoControl(hcj,
 						  FSCTL_QUERY_USN_JOURNAL,
-						  NULL,
+						  NULL, //不需要输入信息
 						  0,
-						  &UsnInfo,
-						  sizeof(UsnInfo),
-						  &br,
+						  &UsnInfo,		   //输出信息存储在UsnInfo中
+						  sizeof(UsnInfo), //大小
+						  &br,			   //实际大小，貌似并无卵用
 						  NULL);
 	if (0 == ret)
 		return -1;
-	if (flag)
+	if (flag) //此处只需要返回64位ID以作校验即可
+	{
+		CloseHandle(hcj); //不要忘了关闭句柄
 		return UsnInfo.UsnJournalID;
-	MFT_ENUM_DATA med;
+	}
+
+	MFT_ENUM_DATA med; //遍历MFT信息，存储了
 	med.StartFileReferenceNumber = 0;
-	med.LowUsn = 0; //UsnInfo.FirstUsn;
-	med.HighUsn = UsnInfo.NextUsn;
-	CHAR buffer[BUF_LEN]; //用于储存记录的缓冲,尽量足够地大
+	med.LowUsn = 0;				   //UsnInfo.FirstUsn;
+	med.HighUsn = UsnInfo.NextUsn; //此处的NextUsn指的是下一条可以写入的USN，也就是最大的USN，区分MaxUsn表示Change Journal能够表示的最大USN
+	CHAR buffer[BUF_LEN];		   //缓冲区
 	DWORD usnDataSize;
-	PUSN_RECORD UsnRecord;
-	long long cnt = 0;
+	PUSN_RECORD UsnRecord; //定义指针类型USNRecord,从Buffer中读取数据
 	ofstream fout(data_dst.c_str(), ios::binary);
 	while (0 != DeviceIoControl(hcj,
 								FSCTL_ENUM_USN_DATA,
@@ -95,6 +98,7 @@ USN GET_MFT(string data_dst, string s, bool flag, vector<dat> &v)
 			int strLen = UsnRecord->FileNameLength;
 			char fileName[MAX_PATH] = {0};
 			WideCharToMultiByte(CP_OEMCP, NULL, UsnRecord->FileName, strLen / 2, fileName, strLen, NULL, FALSE);
+			//将wchar的FileName转换成char数组，便于存储
 			int rnum = (int)UsnRecord->FileReferenceNumber, prnum = (int)UsnRecord->ParentFileReferenceNumber;
 			int len = strlen(fileName) + 1;
 			fout.write((char *)&rnum, sizeof(int));
@@ -105,13 +109,13 @@ USN GET_MFT(string data_dst, string s, bool flag, vector<dat> &v)
 			char *pathBuffer[BUF_LEN];
 			DWORD recordLen = UsnRecord->RecordLength;
 			dwRetBytes -= recordLen;
-			UsnRecord = (PUSN_RECORD)(((PCHAR)UsnRecord) + recordLen);
+			UsnRecord = (PUSN_RECORD)(((PCHAR)UsnRecord) + recordLen); //指针偏移recordLen的量，访问下一条记录
 		}
-		med.StartFileReferenceNumber = *(USN *)&buffer;
+		med.StartFileReferenceNumber = *(USN *)&buffer; //更新读取的起始USN
 	}
 	int end = -1;
 	fout.write((char *)&end, sizeof(int));
-	CloseHandle(hcj);
+	CloseHandle(hcj); //
 	fout.close();
 	return UsnInfo.UsnJournalID;
 }
@@ -121,7 +125,14 @@ bool is_ntfs(const char *drv)
 	HANDLE hVol = INVALID_HANDLE_VALUE;
 	char FileSystemName[MAX_PATH + 1];
 	DWORD MaximumComponentLength;
-	if (GetVolumeInformationA((std::string(drv) + ":\\").c_str(), 0, 0, 0, &MaximumComponentLength, 0, FileSystemName, MAX_PATH + 1) && 0 == strcmp(FileSystemName, "NTFS"))
+	if (GetVolumeInformationA((std::string(drv) + ":\\").c_str(),
+							  0,
+							  0,
+							  0,
+							  &MaximumComponentLength,
+							  0,
+							  FileSystemName, MAX_PATH + 1) &&
+		0 == strcmp(FileSystemName, "NTFS"))
 		ret = 1;
 	CloseHandle(hVol);
 	return ret;
@@ -133,62 +144,71 @@ USN GET_USN(const char *drvname, USN startUSN, bool flag, vector<int> &rm, vecto
 	USN ret = 0;
 	char FileSystemName[MAX_PATH + 1];
 	DWORD MaximumComponentLength;
-	if (GetVolumeInformationA((std::string(drvname) + ":\\").c_str(), 0, 0, 0, &MaximumComponentLength, 0, FileSystemName, MAX_PATH + 1) && 0 == strcmp(FileSystemName, "NTFS")) // 判断是否为 NTFS 格式
+	if (!GetVolumeInformationA((std::string(drvname) + ":\\").c_str(),
+							   0,
+							   0,
+							   0,
+							   &MaximumComponentLength,
+							   0,
+							   FileSystemName,
+							   MAX_PATH + 1) &&
+		0 == strcmp(FileSystemName, "NTFS")) // 判断是否为 NTFS 格式
+		return 0;
+	hVol = CreateFileA((std::string("\\\\.\\") + drvname + ":").c_str(),
+					   GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+	if (hVol == INVALID_HANDLE_VALUE)
+		return 0;
+	DWORD br;
+	USN_JOURNAL_DATA qujd;
+	if (!DeviceIoControl(hVol, FSCTL_QUERY_USN_JOURNAL, NULL, 0, &qujd, sizeof(qujd), &br, NULL))
+		return 0;
+	char buffer[0x1000];
+	DWORD BytesReturned;
+	READ_USN_JOURNAL_DATA rujd = {startUSN,4294967295,0,0,0,qujd.UsnJournalID};
+	while (DeviceIoControl(hVol,
+						   FSCTL_READ_USN_JOURNAL,
+						   &rujd,
+						   sizeof(rujd),
+						   buffer,
+						   _countof(buffer),
+						   &BytesReturned, NULL))
 	{
-		hVol = CreateFileA((std::string("\\\\.\\") + drvname + ":").c_str() // 需要管理员权限，无奈
-						   ,
-						   GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
-		if (hVol != INVALID_HANDLE_VALUE)
+		DWORD dwRetBytes = BytesReturned - sizeof(USN);
+		PUSN_RECORD UsnRecord = (PUSN_RECORD)((PCHAR)buffer + sizeof(USN));
+		if (dwRetBytes == 0)
+			break;
+		while (dwRetBytes > 0)
 		{
-			DWORD br;
-			USN_JOURNAL_DATA qujd;
-			if (DeviceIoControl(hVol, FSCTL_QUERY_USN_JOURNAL, NULL, 0, &qujd, sizeof(qujd), &br, NULL))
+			const int strLen = UsnRecord->FileNameLength;
+			char fileName[MAX_PATH] = {0};
+			WideCharToMultiByte(CP_OEMCP, NULL, UsnRecord->FileName, strLen / 2, fileName, strLen, NULL, FALSE);
+			ret = UsnRecord->Usn;
+			//Reason 256 表示创建 4096 表示删除 8192表示重命名
+			if (flag)
 			{
-				char buffer[0x1000];
-				DWORD BytesReturned;
+				if (UsnRecord->Reason == 256)
+					cr.emplace_back(fileName, (int)UsnRecord->FileReferenceNumber, (int)UsnRecord->ParentFileReferenceNumber, strLen + 1, 0);
+				else if (UsnRecord->Reason == 4096)
+					rm.push_back((int)UsnRecord->FileReferenceNumber);
+				else if (UsnRecord->Reason == 8192)
 				{
-					READ_USN_JOURNAL_DATA rujd = {startUSN, 4294967295, 0, 0, 0, qujd.UsnJournalID};
-					for (; DeviceIoControl(hVol, FSCTL_READ_USN_JOURNAL, &rujd, sizeof(rujd), buffer, _countof(buffer), &BytesReturned, NULL); rujd.StartUsn = *(USN *)&buffer)
+					int tmp = renamed[(int)UsnRecord->FileReferenceNumber];
+					if (!tmp)
 					{
-						DWORD dwRetBytes = BytesReturned - sizeof(USN);
-						PUSN_RECORD UsnRecord = (PUSN_RECORD)((PCHAR)buffer + sizeof(USN));
-						if (dwRetBytes == 0)
-							break;
-						while (dwRetBytes > 0)
-						{
-							const int strLen = UsnRecord->FileNameLength;
-							char fileName[MAX_PATH] = {0};
-							WideCharToMultiByte(CP_OEMCP, NULL, UsnRecord->FileName, strLen / 2, fileName, strLen, NULL, FALSE);
-							ret = UsnRecord->Usn;
-							//Reason 256 表示创建 4096 表示删除
-							if (flag)
-							{
-								if (UsnRecord->Reason == 256)
-									cr.emplace_back(fileName, (int)UsnRecord->FileReferenceNumber, (int)UsnRecord->ParentFileReferenceNumber, strLen + 1, 0);
-								else if (UsnRecord->Reason == 4096)
-									rm.push_back((int)UsnRecord->FileReferenceNumber);
-								else if (UsnRecord->Reason == 8192)
-								{
-									int tmp = renamed[(int)UsnRecord->FileReferenceNumber];
-									if (!tmp)
-									{
-										renamed[(int)UsnRecord->FileReferenceNumber] = ++cnt;
-										tmp = cnt;
-										new_name.push_back(fileName);
-									}
-									else
-										new_name[tmp - 1] = fileName;
-								}
-							}
-							dwRetBytes -= UsnRecord->RecordLength;
-							UsnRecord = (PUSN_RECORD)((PCHAR)UsnRecord + UsnRecord->RecordLength);
-						}
+						renamed[(int)UsnRecord->FileReferenceNumber] = ++cnt;
+						tmp = cnt;
+						new_name.push_back(fileName);
 					}
+					else
+						new_name[tmp - 1] = fileName;
 				}
 			}
-			CloseHandle(hVol);
+			dwRetBytes -= UsnRecord->RecordLength;
+			UsnRecord = (PUSN_RECORD)((PCHAR)UsnRecord + UsnRecord->RecordLength);
 		}
+		rujd.StartUsn = *(USN *)&buffer;
 	}
+	CloseHandle(hVol);
 
 	return ret;
 }
